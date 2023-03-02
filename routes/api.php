8e8b2ebb7b30277ca5d4852df8c6822589e68891
Mohammed Tirichine <mohtiri718@gmail.com>
2023-03-02 14:44:25 +0100

    Cleaning project

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BoardingCardController;
+use App\Services\BoardingCardService;
 
 
 /*
@@ -24,7 +25,7 @@ Route::get('/boarding-cards/sort', [BoardingCardController::class, 'sort']);
  * Boarding cards CRUD operations.
  */
 Route::get('/boarding-cards', [BoardingCardController::class, 'index']);
-Route::get('/boarding-cards/{boarding_card}', [BoardingCardController::class, 'get'])->missing([BoardingCardController::class, 'missing']);
+Route::get('/boarding-cards/{boarding_card}', [BoardingCardController::class, 'get'])->missing([BoardingCardService::class, 'missing']);
 Route::post('/boarding-cards', [BoardingCardController::class, 'store']);
-Route::put('/boarding-cards/{boarding_card}', [BoardingCardController::class, 'update'])->missing([BoardingCardController::class, 'missing']);
-Route::delete('/boarding-cards/{boarding_card}', [BoardingCardController::class, 'destroy'])->missing([BoardingCardController::class, 'missing']);
+Route::put('/boarding-cards/{boarding_card}', [BoardingCardController::class, 'update'])->missing([BoardingCardService::class, 'missing']);
+Route::delete('/boarding-cards/{boarding_card}', [BoardingCardController::class, 'destroy'])->missing([BoardingCardService::class, 'missing']);
